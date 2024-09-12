41735bec6406ec6ca372bbaaa32568fa67caaad8
Kanoyitsuha <hongxun.wie@gmail.com>
2024-09-12 16:13:50 +0800

    Solve Enemy On Next Room Would Die After We Use Charm

--- TeamProject/Assets/Door.cs
+++ TeamProject/Assets/Door.cs
@@ -8,12 +8,18 @@ public class Door : MonoBehaviour
     [SerializeField] private CameraController cam;
     [SerializeField] private GameObject doorSprite;
     [SerializeField] private Enemy[] enemies;
+    [SerializeField] private GameObject[] enemyToActive;
 
     private int enemiesRemaining;
 
     private void Start()
     {
         doorSprite.SetActive(false);
+        foreach (GameObject enemyToActives in enemyToActive)
+        {
+            enemyToActives.SetActive(false);
+
+        }
         enemiesRemaining = enemies.Length;
 
         foreach (Enemy enemy in enemies)
@@ -45,6 +51,17 @@ public class Door : MonoBehaviour
         if (collision.CompareTag("Player") && doorSprite.activeInHierarchy)
         {
             cam.MoveToNewRoom(nextRoom);
+            ActivateEnemys();
         }
+    }
+
+    private void ActivateEnemys()
+    {
+        foreach (GameObject enemyToActives in enemyToActive)
+        {
+            enemyToActives.SetActive(true);
+        }
+
+
     }
 }
